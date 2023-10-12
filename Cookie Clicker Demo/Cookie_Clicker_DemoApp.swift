d47c9c758858d7823d8c4a86019cbbdd66c73ede
David Williams <davidlwilliams423@gmail.com>
2023-10-12 15:30:01 -0400

    Initial Commit

---
+++ Cookie Clicker Demo/Cookie_Clicker_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cookie_Clicker_DemoApp.swift
+//  Cookie Clicker Demo
+//
+//  Created by David Williams on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Cookie_Clicker_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
